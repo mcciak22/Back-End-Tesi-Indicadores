ca13575d238e1eb2e04f8b339a8cc9fc3a52d533
Mario Rodriguez <rodriguez.mario@cuallisys.com>
2020-11-13 14:59:43 -0600

    inicializando el servidor de node

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,16 @@
 import express = require('express');
-
+//inicializando el servidor de node
 //esta clase se importad por default
 export default class Server {
 
-    // public app:express.
-    // public port: number;
+    public aplicacion:express.Application;
+    public puerto: number;
+
+    constructor(
+        puerto: number
+    ){
+        this.puerto = puerto;
+        this.aplicacion = express();
+
+    }
 }
