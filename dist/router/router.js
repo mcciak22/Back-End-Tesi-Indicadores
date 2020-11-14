0c29b06c0ffa0ebc2d75c25007c269c9389ec485
Mario Rodriguez <rodriguez.mario@cuallisys.com>
2020-11-13 20:30:07 -0600

    api de usuario por id

--- dist/router/router.js
+++ dist/router/router.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var mysql_1 = __importDefault(require("../mysql/mysql"));
 var router = express_1.Router();
-router.get('/heroes', function (req, res) {
+router.get('/usuarios', function (req, res) {
     var query = "\n    SELECT *\n    FROM usuarios";
     mysql_1.default.EjecutarQuery(query, function (error, usuarios) {
         if (error) {
@@ -27,12 +27,29 @@ router.get('/heroes', function (req, res) {
     //     mensaje:'todo esta bien api'
     // })
 });
-router.get('/heroes/:id', function (req, res) {
+router.get('/usuario/:id', function (req, res) {
+    //este es el parametro que se consulta desde la url
     var id = req.params.id;
-    res.json({
-        ok: true,
-        mensaje: 'todo esta bien api',
-        id: id
+    var escaparcaracteres = mysql_1.default.instancia.conexion.escape(id);
+    // res.json({
+    //     ok:true,
+    //     mensaje:'todo esta bien api',
+    //     id: id
+    // })
+    var query = "\n    SELECT *\n    FROM usuarios\n    WHERE id_usuario = " + escaparcaracteres;
+    mysql_1.default.EjecutarQuery(query, function (error, usuario) {
+        if (error) {
+            res.status(400).json({
+                ok: false,
+                errorms: error
+            });
+        }
+        else {
+            res.json({
+                ok: true,
+                usuario: usuario[0]
+            });
+        }
     });
 });
 exports.default = router;
--- src/router/router.ts
+++ src/router/router.ts
@@ -34,15 +34,40 @@ router.get('/usuarios',(req:Request,res:Response)=>{
     // })
 })
 
-router.get('/usuarios/:id',(req:Request,res:Response)=>{
+router.get('/usuario/:id',(req:Request,res:Response)=>{
 
+    //este es el parametro que se consulta desde la url
     const id = req.params.id
+    const escaparcaracteres = MySQL.instancia.conexion.escape(id)
+    // res.json({
+    //     ok:true,
+    //     mensaje:'todo esta bien api',
+    //     id: id
+    // })
 
-    res.json({
-        ok:true,
-        mensaje:'todo esta bien api',
-        id: id
-    })
+    const query =`
+    SELECT *
+    FROM usuarios
+    WHERE id_usuario = ${escaparcaracteres}`;
+
+    MySQL.EjecutarQuery(query, (error:any, usuario:Object[] )=>{
+
+        if(error){
+
+            res.status(400).json({
+                ok: false,
+                errorms:error
+                                
+            });
+
+        }else{
+            res.json({
+                ok:true,
+                usuario: usuario[0]
+            })
+        }
+
+    });
 })
 
 export default router; 
