be88b8ee2a06babb1433bbde8762f184a20bc8ad
Mario Rodriguez <rodriguez.mario@cuallisys.com>
2020-11-13 18:56:53 -0600

    servidor corriendo sin problemas

--- dist/server/server.js
+++ dist/server/server.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var path = require("path");
 //inicializando el servidor de node
 //esta clase se importad por default
 var Server = /** @class */ (function () {
@@ -12,8 +13,14 @@ var Server = /** @class */ (function () {
     Server.init = function (puerto) {
         return new Server(puerto);
     };
+    Server.prototype.publicfolder = function () {
+        var publicpath = path.resolve(__dirname, '../public');
+        this.aplicacion.use(express.static(publicpath));
+    };
     Server.prototype.start = function (callback) {
+        //servidor escuchando.
         this.aplicacion.listen(this.puerto, callback);
+        this.publicfolder();
     };
     return Server;
 }());
